01d0e273ca4507af205e28932ed4dde52e8de7de
tanpengsccd <tanpengcd@gmail.com>
2024-07-26 13:16:29 +0800

    feat: 添加ali ecs cdt 远程关机开机功能+1

--- ali-cdt-traffic-auto-shutdown.sh
+++ ali-cdt-traffic-auto-shutdown.sh
@@ -2,14 +2,14 @@
 # 先设置 打开 https://cdt.console.aliyun.com/overview  的调试
 # 找到请求 https://cdt.console.aliyun.com/data/api.json?_fetcher_=cdt__ListCdtInternetTraffic&product=cdt&action=ListCdtInternetTraffic
 # 获取 其 cookie 和 请求体
-# export ALICookie='_samesite_flag_=true; cookie2=XXXXXX;.....'
+# export ALICookie='cookie: _samesite_flag_=true; cookie2=XXXXXX;.....'
 # export ALICDTBody='sec_token=xxxxxxxxxxx'
 # export ALIECSStartBody='collina=140.....'
 # export ALIECSStopBody='collina=140....'
 
 # 使用提供的 curl 命令获取数据
 json_response=$(curl 'https://cdt.console.aliyun.com/data/api.json?_fetcher_=cdt__ListCdtInternetTraffic&product=cdt&action=ListCdtInternetTraffic' \
-    -H "cookie: $ALICDTCookie" \
+    -H "$ALICDTCookie" \
     --data-raw "$ALICDTBody" | jq '.')
 
 # 解析 JSON 响应并检查 Traffic 值
@@ -19,7 +19,7 @@ traffic_in_gb=$(echo "scale=2; $traffic_value/1024/1024/1024" | bc)
 if [ "$traffic_value" -gt 193273528320 ]; then
     echo "当月CDT流量:$traffic_in_gb ,已超过180G"
     curl 'https://ecs.console.aliyun.com/instance/operator/stopInstance.json' \
-        -H "cookie: $ALICDTCookie" \
+        -H "$ALICDTCookie" \
         --data-raw "$ALIECSStopBody"
     echo "关机成功"
 # 如果 == 0 说明流量重置了, 客服说是每月1日重置的, 所以需要开机
@@ -27,7 +27,7 @@ elif [ "$traffic_value" -eq 0 ]; then
 
     echo "当月CDT流量$traffic_in_gb ,已经恢复流量"
     curl 'https://ecs.console.aliyun.com/instance/operator/startInstance.json' \
-        -H "cookie: $ALICDTCookie" \
+        -H "$ALICDTCookie" \
         --data-raw "$ALIECSStartBody"
     echo "开机成功"
 
