c75d86abe6b333e1c53ec4818b2972a83a31e040
tanpengsccd <tanpengcd@gmail.com>
2024-07-26 13:32:28 +0800

    feat: 添加ali ecs cdt 远程关机开机功能+3

--- ali-cdt-traffic-auto-shutdown.sh
+++ ali-cdt-traffic-auto-shutdown.sh
@@ -2,8 +2,10 @@
 # 先设置 打开 https://cdt.console.aliyun.com/overview  的调试
 # 找到请求 https://cdt.console.aliyun.com/data/api.json?_fetcher_=cdt__ListCdtInternetTraffic&product=cdt&action=ListCdtInternetTraffic
 # 获取 其 cookie 和 请求体
-# export ALICookie='cookie: _samesite_flag_=true; cookie2=XXXXXX;.....'
+# export ALICDTCookie='cookie: _samesite_flag_=true; cookie2=XXXXXX;.....'
 # export ALICDTBody='sec_token=xxxxxxxxxxx'
+
+# export ALIECSCookie='cookie: _samesite_flag_=true; cookie2=XXXXXX;.....'
 # export ALIECSStartBody='collina=140.....'
 # export ALIECSStopBody='collina=140....'
 
@@ -19,7 +21,7 @@ traffic_in_gb=$(echo "scale=2; $traffic_value/1024/1024/1024" | bc)
 if [ "$traffic_value" -gt 193273528320 ]; then
     echo "当月CDT流量:$traffic_in_gb ,已超过180G"
     curl 'https://ecs.console.aliyun.com/instance/operator/stopInstance.json' \
-        -H "$ALICookie" \
+        -H "$ALIECSCookie" \
         --data-raw "$ALIECSStopBody"
     echo "关机成功"
 # 如果 == 0 说明流量重置了, 客服说是每月1日重置的, 所以需要开机
@@ -27,7 +29,7 @@ elif [ "$traffic_value" -eq 0 ]; then
 
     echo "当月CDT流量$traffic_in_gb ,已经恢复流量"
     curl 'https://ecs.console.aliyun.com/instance/operator/startInstance.json' \
-        -H "$ALICookie" \
+        -H "$ALIECSCookie" \
         --data-raw "$ALIECSStartBody"
     echo "开机成功"
 
