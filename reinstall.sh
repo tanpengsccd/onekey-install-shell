edb4874cfff8ca14a0ffc6c4fd3f24f2936a3df2
tanpengsccd <tanpengcd@gmail.com>
2025-10-09 01:28:24 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,4 +1,4 @@
-# https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh 基础上添加了 microsoft.gzip 
+# https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh 基础上添加了 microsoft.gzip  改为清华源
 #!/usr/bin/env bash
 # nixos 默认的配置不会生成 /bin/bash
 # shellcheck disable=SC2086
@@ -1116,7 +1116,7 @@ setos() {
 
         # 不要用https 因为甲骨文云arm initramfs阶段不会从硬件同步时钟，导致访问https出错
         if is_in_china; then
-            mirror=http://mirror.nju.edu.cn/alpine/v$releasever
+            mirror=http://mirrors.tuna.tsinghua.edu.cn/alpine/v$releasever
         else
             mirror=http://dl-cdn.alpinelinux.org/alpine/v$releasever
         fi
@@ -1166,8 +1166,8 @@ Continue?
                 # https://github.com/tuna/issues/issues/1999
                 # nju 也没同步
                 udeb_mirror=deb.freexian.com/extended-lts
-                deb_mirror=mirror.nju.edu.cn/debian-elts
-                initrd_mirror=mirror.nju.edu.cn/debian-archive/debian
+                deb_mirror=mirrors.tuna.tsinghua.edu.cn/debian-elts
+                initrd_mirror=mirrors.tuna.tsinghua.edu.cn/debian-archive/debian
             else
                 # 按道理不应该用官方源，但找不到其他源
                 udeb_mirror=deb.freexian.com/extended-lts
@@ -1178,7 +1178,7 @@ Continue?
             if is_in_china; then
                 # ftp.cn.debian.org 不在国内还严重丢包
                 # https://www.itdog.cn/ping/ftp.cn.debian.org
-                mirror=mirror.nju.edu.cn/debian
+                mirror=mirrors.tuna.tsinghua.edu.cn/debian
             else
                 mirror=deb.debian.org/debian # fastly
             fi
@@ -1189,7 +1189,7 @@ Continue?
 
         # 云镜像和 firmware 下载源
         if is_in_china; then
-            cdimage_mirror=https://mirror.nju.edu.cn/debian-cdimage
+            cdimage_mirror=https://mirrors.tuna.tsinghua.edu.cn/debian-cdimage
         else
             cdimage_mirror=https://cdimage.debian.org/images # 在瑞典，不是 cdn
             # cloud.debian.org 同样在瑞典，不是 cdn
@@ -1235,7 +1235,7 @@ Continue?
         else
             # 传统安装
             if is_in_china; then
-                hostname=mirror.nju.edu.cn
+                hostname=mirrors.tuna.tsinghua.edu.cn
             else
                 # http.kali.org 没有 ipv6 地址
                 # http.kali.org (geoip 重定向) 到 kali.download (cf)
@@ -1272,10 +1272,10 @@ Continue?
                 # 有的源没有 releases 镜像
                 # https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images/releases/
                 #   https://unicom.mirrors.ustc.edu.cn/ubuntu-cloud-images/releases/
-                #            https://mirror.nju.edu.cn/ubuntu-cloud-images/releases/
+                #            https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images/releases/
 
                 # mirrors.cloud.tencent.com
-                ci_mirror=https://mirror.nju.edu.cn/ubuntu-cloud-images
+                ci_mirror=https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cloud-images
             else
                 ci_mirror=https://cloud-images.ubuntu.com
             fi
@@ -1310,8 +1310,8 @@ Continue?
             # 传统安装
             if is_in_china; then
                 case "$basearch" in
-                "x86_64") mirror=https://mirror.nju.edu.cn/ubuntu-releases/$releasever ;;
-                "aarch64") mirror=https://mirror.nju.edu.cn/ubuntu-cdimage/releases/$releasever/release ;;
+                "x86_64") mirror=https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/$releasever ;;
+                "aarch64") mirror=https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cdimage/releases/$releasever/release ;;
                 esac
             else
                 case "$basearch" in
@@ -1337,13 +1337,13 @@ Continue?
     setos_arch() {
         if [ "$basearch" = "x86_64" ]; then
             if is_in_china; then
-                mirror=https://mirror.nju.edu.cn/archlinux
+                mirror=https://mirrors.tuna.tsinghua.edu.cn/archlinux
             else
                 mirror=https://geo.mirror.pkgbuild.com # geoip
             fi
         else
             if is_in_china; then
-                mirror=https://mirror.nju.edu.cn/archlinuxarm
+                mirror=https://mirrors.tuna.tsinghua.edu.cn/archlinuxarm
             else
                 # https 证书有问题
                 mirror=http://mirror.archlinuxarm.org # geoip
@@ -1366,7 +1366,7 @@ Continue?
 
     setos_nixos() {
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/nix-channels
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/nix-channels
         else
             mirror=https://nixos.org/channels
         fi
@@ -1384,7 +1384,7 @@ Continue?
 
     setos_gentoo() {
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/gentoo
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/gentoo
         else
             mirror=https://distfiles.gentoo.org # cdn77
         fi
@@ -1421,7 +1421,7 @@ Continue?
         # https://mirrors.tuna.tsinghua.edu.cn/opensuse/ports/aarch64/tumbleweed/appliances/
 
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/opensuse
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/opensuse
         else
             mirror=https://downloadcontentcdn.opensuse.org
         fi
@@ -1607,7 +1607,7 @@ Continue with DD?
 
     setos_aosc() {
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/anthon/aosc-os
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/anthon/aosc-os
         else
             # 服务器在香港
             mirror=https://releases.aosc.io
@@ -1640,10 +1640,10 @@ Continue with DD?
             # ci
             if is_in_china; then
                 case $distro in
-                centos) ci_mirror="https://mirror.nju.edu.cn/centos-cloud/centos" ;;
-                almalinux) ci_mirror="https://mirror.nju.edu.cn/almalinux/$releasever/cloud/$elarch/images" ;;
-                rocky) ci_mirror="https://mirror.nju.edu.cn/rocky/$releasever/images/$elarch" ;;
-                fedora) ci_mirror="https://mirror.nju.edu.cn/fedora/releases/$releasever/Cloud/$elarch/images" ;;
+                centos) ci_mirror="https://mirrors.tuna.tsinghua.edu.cn/centos-cloud/centos" ;;
+                almalinux) ci_mirror="https://mirrors.tuna.tsinghua.edu.cn/almalinux/$releasever/cloud/$elarch/images" ;;
+                rocky) ci_mirror="https://mirrors.tuna.tsinghua.edu.cn/rocky/$releasever/images/$elarch" ;;
+                fedora) ci_mirror="https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/$releasever/Cloud/$elarch/images" ;;
                 esac
             else
                 case $distro in
@@ -2784,7 +2784,7 @@ install_grub_linux_efi() {
         fedora_ver=$(get_latest_distro_releasever fedora)
 
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/fedora
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/fedora
         else
             mirror=https://d2lzkl7pfhq30w.cloudfront.net/pub/fedora/linux
         fi
@@ -2792,7 +2792,7 @@ install_grub_linux_efi() {
         curl -Lo $tmp/$grub_efi $mirror/releases/$fedora_ver/Everything/$basearch/os/EFI/BOOT/$grub_efi
     else
         if is_in_china; then
-            mirror=https://mirror.nju.edu.cn/opensuse
+            mirror=https://mirrors.tuna.tsinghua.edu.cn/opensuse
         else
             mirror=https://downloadcontentcdn.opensuse.org
         fi
@@ -2811,7 +2811,7 @@ download_and_extract_apk() {
     local extract_dir=$3
 
     install_pkg tar xz
-    is_in_china && mirror=http://mirror.nju.edu.cn/alpine || mirror=https://dl-cdn.alpinelinux.org/alpine
+    is_in_china && mirror=http://mirrors.tuna.tsinghua.edu.cn/alpine || mirror=https://dl-cdn.alpinelinux.org/alpine
     package_apk=$(curl -L $mirror/v$alpine_ver/main/$basearch/ | grep -oP "$package-[^-]*-[^-]*\.apk" | sort -u)
     if ! [ "$(wc -l <<<"$package_apk")" -eq 1 ]; then
         error_and_exit "find no/multi apks."
@@ -2829,7 +2829,7 @@ install_grub_win() {
     grub_ver=2.06
     # ftpmirror.gnu.org 是 geoip 重定向，不是 cdn
     # 有可能重定义到一个拉黑了部分 IP 的服务器
-    is_in_china && grub_url=https://mirror.nju.edu.cn/gnu/grub/grub-$grub_ver-for-windows.zip ||
+    is_in_china && grub_url=https://mirrors.tuna.tsinghua.edu.cn/gnu/grub/grub-$grub_ver-for-windows.zip ||
         grub_url=https://mirrors.kernel.org/gnu/grub/grub-$grub_ver-for-windows.zip
     curl -Lo $tmp/grub.zip $grub_url
     # unzip -qo $tmp/grub.zip
@@ -2882,7 +2882,7 @@ install_grub_win() {
         if false; then
             # g2ldr.mbr
             # 部分国内机无法访问 ftp.cn.debian.org
-            is_in_china && host=mirror.nju.edu.cn || host=deb.debian.org
+            is_in_china && host=mirrors.tuna.tsinghua.edu.cn || host=deb.debian.org
             curl -LO http://$host/debian/tools/win32-loader/stable/win32-loader.exe
             7z x win32-loader.exe 'g2ldr.mbr' -o$tmp/win32-loader -r -y -bso0
             find $tmp/win32-loader -name 'g2ldr.mbr' -exec cp {} /cygdrive/$c/ \;
